142f2e37102d79a6a2a7fa28bbcd906ace6aaf71
Houser97 <arturo.riverar97@gmail.com>
2022-08-27 14:12:45 -0500

    Sanitize username and pwd in post controller of sign-up process

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 let mongoose = require('mongoose');
+// PASSPORT
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,17 @@ let async = require('async');
 let {body, validationResult} = require('express-validator');
 
 /* Mostrar formulario de registro */
-exports.user_create_get = function(req, res, next){
-    res.render('sign_up', {
-        title: 'Sign up'
-    })
-}
+exports.user_create_post = [
+    body('username', 'Username must be an email address.').isEmail()
+                .trim()
+                .escape()
+                .normalizeEmail(),
+    body('pwd', 'Password must not be empty').isLength({min: 8})
+                .withMessage('Password must be at least 8 characters')
+                .matches('[0-9]')
+                .withMessage('Password must contain at least 1 number')
+                .matches('[A-Z]')
+                .withMessage('Password must contain at least 1 uppercase letter')
+                .trim()
+                .escape(),
+];
