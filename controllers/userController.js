6968c0d41336885c1e386390e27f1d08de70c352
Houser97 <arturo.riverar97@gmail.com>
2022-08-26 21:33:19 -0500

    Create controller to render sign-up template

---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+let User = require('../models/user');
+let async = require('async');
+let {body, validationResult} = require('express-validator');
+
+/* Mostrar formulario de registro */
+exports.user_create_get = function(req, res, next){
+    res.render('sign-up', {
+        title: 'Sign up'
+    })
+}
