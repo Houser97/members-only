1e281c2f970d822557788080bea1ee9588afda9d
Houser97 <arturo.riverar97@gmail.com>
2022-08-26 20:35:50 -0500

    Create user.js file to create User model

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    role: {type: String, required: true},
+    messages: {type: Schema.Types.ObjectId, ref: 'Message'}
+});
+
+module.exports = mongoose.model('User', userSchema);
