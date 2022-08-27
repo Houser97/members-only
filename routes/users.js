87194b03932fc415f620f3ab00fc6506aa15486f
Houser97 <arturo.riverar97@gmail.com>
2022-08-26 21:26:31 -0500

    Create routes for the user model

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* Mostrar información del usuario */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* Formulario para registrarse */
+router.get('/sign-up', function(req, res, next){})
 
 module.exports = router;
