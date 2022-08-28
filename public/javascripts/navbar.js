cee3226adde30bc1590364641af76d54a2c9e5ba
Houser97 <arturo.riverar97@gmail.com>
2022-08-28 08:16:51 -0500

    Add functionality tu nav's buttons to open forms

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -15,3 +15,22 @@ svgCloseMenu.addEventListener('click', () => {
     svgOpenMenu.style.display = 'flex';
     navbar.style.transform = 'translateX(100%)'
 })
+
+// Abrir formularios
+const logInButtonNav = document.querySelector('.log-in-button-nav');
+const signUpButtonNav = document.querySelector('.sign-up-button-nav');
+
+const logInFormFromNav = document.querySelector('.log-in');
+const signUpFormFromNav = document.querySelector('.sign-up');
+
+signUpButtonNav.addEventListener('click', () => {
+    signUpFormFromNav.style.opacity = '1';
+    signUpFormFromNav.style.transform = 'translate(50%, -50%) scale(1)';
+    backgroundDiv.style.display = 'flex';
+})
+
+logInButtonNav.addEventListener('click', ()=>{
+    logInFormFromNav.style.opacity = '1';
+    logInFormFromNav.style.transform = 'translate(50%, -50%) scale(1)';
+    backgroundDiv.style.display = 'flex';
+});
